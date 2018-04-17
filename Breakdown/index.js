fa7df9ce35cee8033f3bbdf99c08218f98ebe5af
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-04-17 20:01:03 +0400

    Collision with paddle

--- Breakdown/index.js
+++ Breakdown/index.js
@@ -4,32 +4,30 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
 var y = canvas.height - 30;
 
-var ballRadius=10;
-var paddleHeight =10;
-var paddleWidth =75;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
 
-var paddleX = (canvas.width-paddleWidth)/2;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
-var rightPressed= false;
+var rightPressed = false;
 var leftPressed = false;
 
-document.addEventListener("keydown",keydownHandler,false);
-document.addEventListener("keyup",keyupHandler,false);
+document.addEventListener("keydown", keydownHandler, false);
+document.addEventListener("keyup", keyupHandler, false);
 
-function keydownHandler(e){
-  if(e.keyCode == 39){
+function keydownHandler(e) {
+  if (e.keyCode == 39) {
     rightPressed = true;
-  }
-  else if(e.keyCode == 37){
+  } else if (e.keyCode == 37) {
     leftPressed = true;
   }
 }
 
-function keyupHandler(e){
-  if(e.keyCode==39){
+function keyupHandler(e) {
+  if (e.keyCode == 39) {
     rightPressed = false;
-  }
-  else if(e.keyCode ==37){
+  } else if (e.keyCode == 37) {
     leftPressed = false;
   }
 }
@@ -50,9 +48,9 @@ function drawBall() {
   ctx.closePath();
 }
 
-function drawPaddle(){
+function drawPaddle() {
   ctx.beginPath();
-  ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -66,19 +64,32 @@ function draw() {
   x += dx;
   y += dy;
 
-  if(y+dy > canvas.height - ballRadius|| y+dy < ballRadius){
-    dy =-dy;
-  }
-  if(x+dx>canvas.width|| x+dx <ballRadius){
-    dx= -dx;
+  // bounce ball - top
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  } 
+  // hitting the bottom wall
+  else if (y + dy > canvas.height - ballRadius) {
+    // check if ball within the width of the paddle
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert("GAME OVER");
+      document.location.reload();
+    }
   }
 
-  if(rightPressed&& paddleX < canvas.width - paddleWidth ){
-    paddleX+=7;
+  if (x + dx > canvas.width || x + dx < ballRadius) {
+    dx = -dx;
   }
-  else if (leftPressed && paddleX > 0){
-    paddleX -=7;
+
+  // move paddle
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
+
 }
 
 
