9de684d3bd10a2f6e7239e9c72aed65d4420c316
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-04-17 23:05:03 +0400

    Collision with blocks

--- Breakdown/index.js
+++ Breakdown/index.js
@@ -27,14 +27,15 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
-
+// INITIALISE BRICKS
 var bricks = [];
 for (c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for (r = 0; r < brickRowCount; r++) {
     bricks[c][r] = {
       x: 0,
-      y: 0
+      y: 0,
+      status: 1
     };
   }
 }
@@ -58,22 +59,36 @@ function keyupHandler(e) {
   }
 }
 
+function collisionDetection() {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+        dy = -dy;
+        b.status = 0;
+      }
+    }
+  }
+}
+
 
 function drawBricks() {
   for (c = 0; c < brickColumnCount; c++) {
     for (r = 0; r < brickRowCount; r++) {
-      
-      var brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
-      var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
-      
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
+        if(bricks[c][r].status ==1){
 
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+          var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+          var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#0095DD";
+          ctx.fill();
+          ctx.closePath();
+        }
     }
   }
 }
@@ -100,6 +115,7 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBricks();
   drawBall();
+  collisionDetection();
   drawPaddle();
   x += dx;
   y += dy;
