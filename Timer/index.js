bd2e201d66ee2b1dec55d8673a73bb3c80773ba3
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-04-23 21:18:50 +0400

    add changes to style

--- Timer/index.js
+++ Timer/index.js
@@ -1,19 +1,34 @@
-var min = 25;
-var sec = 0;
+var min;
+var sec;
 
 var minElement = document.getElementById("minute");
 var secElement = document.getElementById("second");
-var btnStartElement = document.getElementById("btn-start")
+var btnStartElement = document.getElementById("btn-start");
+var btnPauseElement = document.getElementById("btn-pause")
+var btnRestartElement = document.getElementById("btn-restart")
 
 btnStartElement.addEventListener("click",start,true);
+btnPauseElement.addEventListener("click",pause,true);
+btnRestartElement.addEventListener("click",restart,true);
+
+var intervalID;
 
 function init(){
+	min =25;
+	sec=0;
 	minElement.innerHTML = min;
-	secElement.innerHTML = sec;
+	secElement.innerHTML = sec+"0";
 }
 
 function start(){
-	setInterval(updateTimer,1000);
+	intervalID = setInterval(updateTimer,1000);
+}
+function pause(){
+    clearInterval(intervalID);
+}
+
+function restart(){
+	init();
 }
 
 function updateTimer(){
